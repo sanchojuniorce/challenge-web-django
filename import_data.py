309b8ab5662efad0976e186836051ba878295ca6
Sancho Júnior <sanchorodriguesnovooriente@gmail.com>
2023-05-28 05:25:55 -0300

    feat: :art: Adjust Sql

--- import_data.py
+++ import_data.py
@@ -4,7 +4,7 @@ from mysql import connector
 from datetime import datetime
 
 FILE = 'openfoodfacts/csv/openfoodfacts_export.csv'
-INSERT = "INSERT INTO challenge_open_food.products (code, status, imported_t, url, creator, created_t, last_modified_t, url, product_name, quantity, brands, categories, labels, cities, purchase_places, stores, ingredients_text, traces, serving_size, serving_quantity, nutriscore_score, nutriscore_grade, main_category, image_url) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+INSERT = "INSERT INTO challenge_open_food.products (code, status, imported_t, url, creator, created_t, last_modified_t, url, product_name, quantity, brands, categories, labels, cities, purchase_places, stores, ingredients_text, traces, serving_size, serving_quantity, nutriscore_score, nutriscore_grade, main_category, image_url) "
 
 def envs_database_mysql():
     return dotenv_values(".env")
@@ -18,11 +18,12 @@ def query_database_mysql(sql, val):
         database=config['DATABASE']
     )
 
-    cur = connection.cursor()
-    query_database = cur.execute("SELECT * FROM challenge_open_food.products;")
-    result = cur.fetchone()
-    connection.close()
-    return result
+    print("{} VALUES ({})".format(sql, val))
+    # cur = connection.cursor()
+    # query_database = cur.execute("{} VALUES ({})".format(sql, values))
+    # result = cur.fetchone()
+    # connection.close()
+    #return result
 
 def read_import_csv():
     arr = []
@@ -40,42 +41,41 @@ def read_import_csv():
                 created_t = datetime.now()
                 last_modified_t = datetime.now()
                 product_name = ' '.join(line.decode('UTF-8').split(' ')[29:35])
-                quantity = line.decode('UTF-8').split(' ')[16::17] #"380 g (6 x 2 u.)"
-                brands =  line.decode('UTF-8').split(' ')[21:22]
-                print(line.decode('UTF-8').split(' ')[0::])
-                categories = line.decode('UTF-8').split(',')[26:27]
-                labels =  line.decode('UTF-8').split(',')[28:29]
-                cities =  ""
+                quantity = ' '.join(line.decode('UTF-8').split(' ')[16::17])
+                brands =  ' '.join(line.decode('UTF-8').split(' ')[21:22])
+                categories = ' '.join(line.decode('UTF-8').split(',')[26:27])
+                labels =  ' '.join(line.decode('UTF-8').split(',')[28:29])
+                cities =  ' '.join(line.decode('UTF-8').split('\t')[50:51][0])
                 purchase_places = ','.join(line.decode('UTF-8').split(',')[33:40])
                 stores = ','.join(line.decode('UTF-8').split(',')[41:42])
-                ingredients_text = "farinha de trigo, açúcar, óleo vegetal de girassol, clara de ovo, ovo, humidificante (sorbitol), levedantes químicos (difosfato dissódico, hidrogenocarbonato de sódio), xarope de glucose-frutose, sal, aroma"
-                traces = "Frutos de casca rija,Leite,Soja,Sementes de sésamo,Produtos à base de sementes de sésamo"
-                serving_size = "madalena 31.7 g"
+                ingredients_text = ','.join(line.decode('UTF-8').split(',')[80:89])
+                traces = ' '.join(line.decode('UTF-8').split('\t')[50:51][0])
+                serving_size = ' '.join(line.decode('UTF-8').split('\t')[59:60][0])
                 serving_quantity = ''
-                nutriscore_score = ''
-                nutriscore_grade = "d"
-                main_category = "en:madeleines"
+                nutriscore_score =  ' '.join(line.decode('UTF-8').split('\t')[103:104])
+                nutriscore_grade = ' '.join(line.decode('UTF-8').split('\t')[104:105])
+                main_category = ' '.join(line.decode('UTF-8').split('\t')[306:307][0])
                 image_url = "https://static.openfoodfacts.org/images/products/{}/front_pt.5.400.jpg".format(code)
                 values.append(
                     {
-                        code, status, imported_t, url, creator, last_modified_t,
-                        product_name, quantity, brands, categories, labels, cities,
-                        purchase_places, stores, ingredients_text, traces, serving_size,
-                        serving_quantity, nutriscore_score, nutriscore_grade, main_category, 
-                        image_url
+                        code, status, imported_t, url, creator, last_modified_t, product_name, quantity, brands,
+                        categories, labels, cities, purchase_places, stores, ingredients_text,
+                        traces, serving_size, serving_quantity, nutriscore_score, 
+                        nutriscore_grade, main_category, image_url
                     }
                 )
             except:
-                print('') 
-    #return values              
-    print(values)            
-            #query_database_mysql(insert, val)
+                print('')  
+            #print(values.sort())            
+            query_database_mysql(INSERT, values.sort())
             #mycursor.execute(insert, val)
-    #print(len(arr))
+            #print(len(arr))
 
-#       mydb.commit()
+            #mydb.commit()
 
-#       print(mycursor.rowcount, "record inserted.")
+            #print(mycursor.rowcount, "record inserted.")
+                  
+        return values
 
 def verify_code(code_string):
     if "\t" in code_string[0]: 
